36ddb47c13fb6d8dcfce41f1f2ddb3c48d0b29dd
Pengnn <15801891351@163.com>
2021-03-06 16:25:26 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,19 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+    //查询帖子
+    List<DiscussPost> selectDiscussPosts(int userId,int offset,int limit);
+
+    //查询帖子的行数
+    //@Param用于给参数起别名:如果只有一个参数，并且在<if>中使用，必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+}
